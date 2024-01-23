cccde548ad86fae927339657775ff60495f86eb0
belenespilman <belen.espilman@gmail.com>
2024-01-23 16:10:15 +0100

    components setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Camps from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <>
-      
+      <Hero />
+      <Camps />
+      <Guide />
+      <Features />
+      <GetApp />
+
     </>
   
   );
