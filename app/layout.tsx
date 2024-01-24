5d637b0bef86d9e87b249877e7031ca1600fccee
belenespilman <belen.espilman@gmail.com>
2024-01-24 20:24:08 +0100

    Add prettier and format files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,26 @@
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
-import type { Metadata } from 'next';
-import "./globals.css";
-
-
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Travel",
-  description: "Travel UI/UX App for Camping",
-};
+  title: 'Travel',
+  description: 'Travel UI/UX App for Camping',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <Navbar />
-        <main className='relative overflow-hidden'>
-          {children}
-          </main>
-        
+        <main className='relative overflow-hidden'>{children}</main>
+
         <Footer />
-        </body>
+      </body>
     </html>
-  );
+  )
 }
